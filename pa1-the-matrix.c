3b7299c80a73164bd2a07c8597f9224f9b828f21
pnavale2409 <navaleprathamesh2409@gmail.com>
2023-09-05 10:49:22 +0530

    prefetch and blocking prefetch implemented

--- pa1-the-matrix.c
+++ pa1-the-matrix.c
@@ -212,54 +212,39 @@ void prefetch_mat_mul(double *A, double *B, double *C, int dim) {
 	for (int i = 0; i < dim; i++) {
 		for (int j = 0; j < dim; j++) {
 			double sum=0;
-			for (int k = 0; k < dim; k=k+20) {				
+			    __builtin_prefetch(&C[i * dim + j],1,3);
+				__builtin_prefetch(&A[i * dim],0,3);
+				__builtin_prefetch(&A[i * dim + 8],0,3);
+				__builtin_prefetch(&B[j], 0,3);
+				__builtin_prefetch(&B[dim + j],0,3);
+				__builtin_prefetch(&B[2 * dim + j],0,3);
+				__builtin_prefetch(&B[3 * dim + j],0,3);
+				__builtin_prefetch(&B[4 * dim + j],0,3);	
+				
+				int k;
+			for (k= 0; k < dim-5; k=k+5) {				
 				__builtin_prefetch(&A[i * dim + k],0,3);
-				__builtin_prefetch(&B[k * dim + j], 0,3);
-				__builtin_prefetch(&B[(k + 1)* dim + j],0,3);
-				__builtin_prefetch(&B[(k + 2) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 3) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 4) * dim + j],0,3);				
-				__builtin_prefetch(&B[(k + 5) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 6) * dim + j],0,3);
+				__builtin_prefetch(&B[(k + 5)* dim + j], 0,3);
+				__builtin_prefetch(&B[(k + 6)* dim + j],0,3);
 				__builtin_prefetch(&B[(k + 7) * dim + j],0,3);
 				__builtin_prefetch(&B[(k + 8) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 9) * dim + j],0,3);
+				__builtin_prefetch(&B[(k + 9) * dim + j],0,3);		
 				__builtin_prefetch(&A[i * dim + k+8],0,3);
-				__builtin_prefetch(&B[(k + 10) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 11) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 12) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 13) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 14) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 15) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 16) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 17) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 18) * dim + j],0,3);
-				__builtin_prefetch(&B[(k + 19) * dim + j],0,3);
-				__builtin_prefetch(&A[i * dim + k+16],0,3);
 			
 				sum += A[i * dim + k] * B[k * dim + j];
 				sum += A[i * dim + k+1] * B[(k + 1)* dim + j];
 				sum += A[i * dim + k+2] * B[(k + 2) * dim + j];
 				sum += A[i * dim + k+3] * B[(k + 3) * dim + j];
-				sum += A[i * dim + k+4] * B[(k + 4) * dim + j];
-				sum += A[i * dim + k+5] * B[(k + 5) * dim + j];
-				sum += A[i * dim + k+6] * B[(k + 6) * dim + j];
-				sum += A[i * dim + k+7] * B[(k + 7) * dim + j];
-				sum += A[i * dim + k+8] * B[(k + 8)* dim + j];
-				sum += A[i * dim + k+9] * B[(k + 9) * dim + j];
-				sum += A[i * dim + k+10] * B[(k + 10) * dim + j];
-				sum += A[i * dim + k+11] * B[(k + 11) * dim + j];
-				sum += A[i * dim + k+12] * B[(k + 12) * dim + j];
-				sum += A[i * dim + k+13] * B[(k + 13) * dim + j];
-				sum += A[i * dim + k+14] * B[(k + 14) * dim + j];
-			
-				sum += A[i * dim + k+15] * B[(k + 15)* dim + j];
-				sum += A[i * dim + k+16] * B[(k + 16) * dim + j];
-				sum += A[i * dim + k+17] * B[(k + 17) * dim + j];
-				sum += A[i * dim + k+18] * B[(k + 18) * dim + j];
-				sum += A[i * dim + k+19] * B[(k + 19) * dim + j];
+				sum += A[i * dim + k+4] * B[(k + 4) * dim + j];				
 				
 			}
+				
+				sum += A[i * dim + k] * B[k * dim + j];
+				sum += A[i * dim + k+1] * B[(k + 1)* dim + j];
+				sum += A[i * dim + k+2] * B[(k + 2) * dim + j];
+				sum += A[i * dim + k+3] * B[(k + 3) * dim + j];
+				sum += A[i * dim + k+4] * B[(k + 4) * dim + j];				
+
 				C[i * dim + j]=sum;
 		}
 	}
@@ -347,6 +332,40 @@ void blocking_simd_mat_mul(double *A, double *B, double *C, int dim, int block_s
  * @note 		The block size should be a multiple of the dimension of the matrices.
 */
 void blocking_prefetch_mat_mul(double *A, double *B, double *C, int dim, int block_size) {
+
+
+	for (int ib = 0; ib < dim; ib+=block_size){
+		for (int jb = 0; jb < dim; jb+=block_size){
+			for (int kb = 0; kb < dim; kb+=block_size){
+
+				for (int i = ib; i < (ib+block_size); i++) {
+					for (int j = jb; j < (jb+block_size); j++) {
+						double sum = C[i * dim + j];
+						int k;
+						for (k = kb; k < (kb+block_size-8); k=k+8) {
+							__builtin_prefetch(&A[i * dim + (k + 8)], 0, 3);
+							__builtin_prefetch(&B[i * dim + (k + 8)], 0, 3);
+							sum += A[i * dim + k] * B[k * dim + j];
+							sum += A[i * dim + (k + 1)] * B[(k + 1) * dim + j];
+							sum += A[i * dim + (k + 2)] * B[(k + 2) * dim + j];
+							sum += A[i * dim + (k + 3)] * B[(k + 3) * dim + j];
+							sum += A[i * dim + (k + 4)] * B[(k + 4) * dim + j];
+							sum += A[i * dim + (k + 5)] * B[(k + 5) * dim + j];
+							sum += A[i * dim + (k + 6)] * B[(k + 6) * dim + j];
+							sum += A[i * dim + (k + 7)] * B[(k + 7) * dim + j];
+							
+						}
+						for (int s = k;s < (kb+block_size); s++){
+							sum += A[i * dim + s] * B[s * dim + j];
+						}
+						C[i * dim + j] = sum;
+					}
+				}
+
+
+			}
+		}
+	}
 
 }
 
